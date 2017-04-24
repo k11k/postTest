70cd39c4a61981e14bf858be81a91d392c9c75ee
User <vinerse@mail.ru>
2017-04-24 15:44:41 +0400

    no message

---
+++ PostTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PostTest
+//
+//  Created by User on 24.04.17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PostTestTests/PostTestTests.m
@@ -0,0 +1,39 @@
+//
+//  PostTestTests.m
+//  PostTestTests
+//
+//  Created by User on 24.04.17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PostTestTests : XCTestCase
+
+@end
+
+@implementation PostTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
